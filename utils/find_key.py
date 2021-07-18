bf87c8ce47cb6218a0f6e46f8232f9b2c793b135
v4rgas <juanvargas@uc.cl>
2021-07-18 19:10:36 -0400

    tryhardeando asos extremos

--- utils/find_key.py
+++ utils/find_key.py
@@ -23,16 +23,15 @@ def find_key(texto, largo_clave, lang):
         max_similarity = 0
         best_match = ''
         for frec_letter, _ in sorted_chunck:
-            possible_key_letter = get_key(frec_letter, frecuent_letters[0])
-
-            letters = [letter for letter, _ in sorted_chunck]
-            letters = ''.join(map(lambda x: decode(x, possible_key_letter), letters))
-
-            ratio = fuzz.partial_ratio(''.join(letters), frecuent_letters)
-            
-            if ratio>max_similarity:
-                max_similarity = ratio
-                best_match = possible_key_letter
+            for frecuent_letter in frecuent_letters:
+                possible_key_letter = get_key(frec_letter, frecuent_letter)
+                letters = [letter for letter, _ in sorted_chunck]
+                letters = ''.join(map(lambda x: decode(x, possible_key_letter), letters))
+                ratio = fuzz.partial_ratio(''.join(letters), frecuent_letters)
+                
+                if ratio>max_similarity:
+                    max_similarity = ratio
+                    best_match = possible_key_letter
         
         possible_key+=best_match
 
