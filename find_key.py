9654ccbc6c28f3c21e9b4fcfb45f7ea0b42a450c
v4rgas <juanvargas@uc.cl>
2021-07-18 14:49:24 -0400

    fixed some stuff

--- find_key.py
+++ find_key.py
@@ -1,42 +1,37 @@
 from string import ascii_uppercase
 from vigenere import encode, decode, get_key
 
-LARGO_CLAVE = 4
+LARGO_CLAVE  = 4
 LETRAS_COMUNES = 'SRNDLC'
-FRECUENT_LETTERS = 'ETAIONSH'
+FRECUENT_LETTERS ='ETAIONSH' 
 
 KEYS = 5
 
-# Devuelve una lista con tuplas, (CARACTER, FRECUENCIA) ordenadas de mayor frecuencia a menor
-
-
+#Devuelve una lista con tuplas, (CARACTER, FRECUENCIA) ordenadas de mayor frecuencia a menor
 def sort_by_frecuency(alist):
     lista_frecuencia = [(char, alist.count(char)) for char in set(alist)]
     lista_frecuencia.sort(key=lambda char: char[1], reverse=True)
-    return lista_frecuencia
+    return lista_frecuencia 
 
 
-with open('example_text.txt') as file:
-    TEXTO = file.read()
+def find_key(self, TEXTO, LARGO_CLAVE, FRECUENT_LETTERS):
+    # Se para el texto en chuncks de tamaño LARGO_CLAVE
+    chuncks = [[letra for index, letra in enumerate(TEXTO) if (index)%(LARGO_CLAVE) == numero] for numero in range(5)]  
 
-# TEXTO = 'ABABABABAB'
+    # LIMPIO CHUNCKS VACIOS
+    chuncks = [chunck for chunck in chuncks if chunck]
 
-chuncks = [[letra for index, letra in enumerate(TEXTO) if (
-    index) % (LARGO_CLAVE) == numero] for numero in range(LARGO_CLAVE)]
+    # BUSCO FRECUENCIA
+    possible_key = ''
+    for chunck in chuncks:
+        sorted_chunck = sort_by_frecuency(chunck)
+        most_frec_letter, _  = sorted_chunck[0]
+        possible_key_letter = get_key(most_frec_letter, FRECUENT_LETTERS[0])
+        
+        possible_key+=possible_key_letter
 
-# LIMPIO CHUNCKS VACIOS
-chuncks = [chunck for chunck in chuncks if chunck]
+    return possible_key
 
-# BUSCO FRECUENCIA
-possible_key = ''
-for chunck in chuncks:
-    sorted_chunck = sort_by_frecuency(chunck)
-    most_frec_letter, most_frec = sorted_chunck[0]
-    possible_key_letter = get_key(most_frec_letter, FRECUENT_LETTERS[0])
-
-    possible_key += possible_key_letter
-
-print(possible_key)
-
-with open('de.txt', 'w') as file:
-    file.write(decode(TEXTO, possible_key))
+if __name__ == '__main__':
+    # with open()
+    pass
