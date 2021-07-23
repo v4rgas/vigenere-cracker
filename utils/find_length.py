813f4ec01d7a0e98ef08eed50737815383a336f3
v4rgas <juanvargas@uc.cl>
2021-07-22 23:18:25 -0400

    quick fix

--- utils/find_length.py
+++ utils/find_length.py
@@ -3,7 +3,7 @@ from functools import reduce
 from itertools import combinations
 from math import gcd
 
-from utils.vigenere import sort_by_frecuency
+# from utils.vigenere import sort_by_frecuency
 
 
 def nomio_finder(texto_codificado: str, length: int):
@@ -37,3 +37,23 @@ def find_length(texto_codificado):
                 divisores.append(gcd_variable)
 
     return sort_by_frecuency(divisores)
+
+
+def coincidence_index(list):
+    pass
+
+
+def find_length_cindex(texto_codificado):
+
+    largo_codificado = len(texto_codificado)
+
+    lista = []
+    for cantidad_grupo in range(largo_codificado):
+        grupos = [[texto_codificado[index] for index in range(
+            largo_codificado) if index % cantidad_grupo == offset] for offset in range(cantidad_grupo)]
+
+        lista.append(grupos)
+
+
+if __name__ == "__main__":
+    find_length_cindex('ABCDEFGHIJK')
