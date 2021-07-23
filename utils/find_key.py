6cfbe27c9cb31c923c9860673c51eaa8a4c17bd1
v4rgas <juanvargas@uc.cl>
2021-07-23 02:02:25 -0400

    comentarios

--- utils/find_key.py
+++ utils/find_key.py
@@ -23,22 +23,33 @@ def find_key(texto, largo_clave, lang) -> str:
     # busco frecuencia
     possible_key = ''
     for chunck in chuncks:
+        # Ordeno por letras mas frecuentes
         sorted_chunck = sort_by_frecuency(chunck)[:len(frecuent_letters)]
 
         max_similarity = 0
         best_match = ''
         for frec_letter, _ in sorted_chunck:
             for frecuent_letter in frecuent_letters:
+
+                # Se generan posibles distribuciones asumiendo que cada una de las letras
+                # mas frecuentes en el texto corresponde a
+                # cada una de las letras mas frecuentes en ingles
+
                 possible_key_letter = get_key(frec_letter, frecuent_letter)
                 letters = [letter for letter, _ in sorted_chunck]
                 letters = ''.join(
                     map(lambda x: decode(x, possible_key_letter)[1], letters))
+
+                # Luego com fuzz.ratio (distancia entre strings) medimos que tan parecidas
+                # son estas distribuciones a las letras más frecuentes en ingles
                 ratio = fuzz.ratio(''.join(letters), frecuent_letters)
 
                 if ratio > max_similarity:
+                    # Nos quedamos con la letra que causa la distribucion más parecida
                     max_similarity = ratio
                     best_match = possible_key_letter
 
+        # Se hace esto por cada columna, por lo que se genera la clave más probable
         possible_key += best_match
 
     return possible_key
